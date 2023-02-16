2ccc854a17a68e75666a0bc3e9550a5b8abbbbb7
Yonas Kahsay <yk8066@gmail.com>
2023-02-16 09:26:23 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: "sizeof(char)" \n");/**string*/
-printf("Size of a int: "sizeof(int)" \n");/**string*/
-printf("Size of a long int: "sizeof(long int)" \n");/**string*/
-printf("Size of a long long int: "sizeof(long long int)" \n");/**string*/
-printf("Size of a float: "sizeof(float)" \n");/**string*/
+printf("Size of a char: ",sizeof(char)," \n");/**string*/
+printf("Size of a int: ",sizeof(int)," \n");/**string*/
+printf("Size of a long int: ",sizeof(long int)," \n");/**string*/
+printf("Size of a long long int: ",sizeof(long long int)," \n");/**string*/
+printf("Size of a float: ",sizeof(float)," \n");/**string*/
 return (0);
 }
