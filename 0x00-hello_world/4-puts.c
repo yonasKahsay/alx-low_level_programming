e9b33501a03568b1b07d7d043385dd89876fd389
Yonas Kahsay <yk8066@gmail.com>
2023-02-16 08:45:30 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /* This will print hello world */
  int main(void)
 {
- char st[] ="Programming is like building a multilingual puzzle";
+ char st[] ="\"Programming is like building a multilingual puzzle";
  puts(st);
  return (0);
 }
