abf29bb600b537571720a03eb4312d35f9d793e5
Yonas Kahsay <yk8066@gmail.com>
2023-02-16 09:21:45 -0800

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: "sizeof(char)" \n");/**string*/
+printf("Size of a int: "sizeof(int)" \n");/**string*/
+printf("Size of a long int: "sizeof(long int)" \n");/**string*/
+printf("Size of a long long int: "sizeof(long long int)" \n");/**string*/
+printf("Size of a float: "sizeof(float)" \n");/**string*/
+return (0);
+}
