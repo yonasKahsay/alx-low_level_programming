7fccbc724dd265e239b91f9c101c1b8338eb1846
Yonas Kahsay <yk8066@gmail.com>
2023-02-16 08:59:43 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/**This will print hello world*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char st[] = "\"Programming is like building a multilingual puzzle";/**string*/
