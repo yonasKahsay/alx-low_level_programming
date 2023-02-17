1981bf7a2604563b86d9376e501378edd4f80507
Yonas Kahsay <yk8066@gmail.com>
2023-02-17 06:34:06 -0800

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,17 +14,17 @@ int L;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 L = n % 10;
-if ( L > 5 )
+if (L > 5)
 {
-printf("Last digit of %d is %d and is greater than 5 \n", n , L);/**string*/
+printf("Last digit of %d is %d and is greater than 5 \n", n, L);/**string*/
 }
-else if ( L == 0 )
+else if  (L == 0)
 {
-printf("Last digit of %d is %d and is 0 \n", n , L);/**string*/
+printf("Last digit of %d is %d and is 0 \n", n, L);/**string*/
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0 \n", n , L);/**string*/
+printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, L);/**string*/
 }
 return (0);
 }
