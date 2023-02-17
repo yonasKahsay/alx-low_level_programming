cd9dc47430f8ac578e84d50f04e0944d4d177ed0
Yonas Kahsay <yk8066@gmail.com>
2023-02-17 07:53:36 -0800

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha[] = "abcdefghijklmnopqrstuvwxyz"
+
+for (i = 0; i < 26; i++)
+{
+putchar(alpha[i]);
+}
+return (0);
+}
