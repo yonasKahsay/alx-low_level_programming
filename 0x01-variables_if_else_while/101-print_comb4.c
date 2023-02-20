96e5d5362d445e42ef09cbc4901108cf01f616a1
Yonas Kahsay <yk8066@gmail.com>
2023-02-20 06:52:37 -0800

    commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+int j;
+
+while (i < 10)
+{
+j = 0;
+j = i + 1;
+while (j < 10)
+{
+k = 0;
+k = j + 1 ;
+while (k < 10) 
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(k + '0');
+if (i == 7 && j == 8 && k == 9)
+{
+k++;
+}
+else
+{
+putchar(',');
+putchar(' ');
+k++;
+}
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
