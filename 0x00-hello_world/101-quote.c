cc0546e4869af8b43dcbb36bcd2b6baabe41fb13
Yonas Kahsay <yk8066@gmail.com>
2023-02-19 23:16:25 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char out = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char[] out = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 putchar(stderr, out);
 return (0);
