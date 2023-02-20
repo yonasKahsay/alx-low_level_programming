5b6419f8fda758674e427a70a19065f1af093f9d
Yonas Kahsay <yk8066@gmail.com>
2023-02-20 00:11:15 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
-return (0);
+fwrite("and that piece of art is useful\" - Dora Korpar, 
+2015-10-19\n", 59, 1, stderr);
+return (1);
 }
