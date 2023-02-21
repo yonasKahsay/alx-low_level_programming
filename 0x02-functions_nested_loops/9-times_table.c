93806cd37e468c80a26059558a0ff9cf6ec39686
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 12:39:45 -0800

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,6 +21,8 @@ void times_table(void)
 		{
 			_putchar(l + '0');
 			_putchar(',');
+			if (j == 9)
+                                goto lastline;
 			_putchar(' ');
 			_putchar(' ');
 			j++;
@@ -31,7 +33,7 @@ void times_table(void)
 			n = (l - m) / 10;
 			_putchar(n + '0');
 			_putchar(m + '0');
-			if (m == 1 && n == 8)
+			if (j == 9)
 				goto lastline;
 			_putchar(',');
 			_putchar(' ');
