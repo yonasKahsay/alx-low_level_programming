5c4218c2992fba61cf4fa4a80e066c1249975eec
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 06:57:58 -0800

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include "main.h"
 /**
  *  _islower - function
  * @c: an inputted letter
  *
- * Return: 0 if it is not lower case 1 if it is uppercase
+ * Return: 0 if it is not lower case, 1 if it is uppercase
  */
 int _islower(int c)
 	{
