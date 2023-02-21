21125c0d09e948e8a24a431fee54b7e12e92aa7f
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 13:15:06 -0800

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <studio.h>
+/**
+ * print_to_98 - function
+ *@n: n is argument
+ *
+ * Return: m as it is the last digit of the no
+ */
+void print_to_98(int n)
+	{
+	if (n <= 98)
+	{
+		while (n < 98)
+		{
+			printf("%d,", n);
+			n++;
+		}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d,", n);
+			n--;
+		}
+	}
+	printf("%d", 98);
+	}
