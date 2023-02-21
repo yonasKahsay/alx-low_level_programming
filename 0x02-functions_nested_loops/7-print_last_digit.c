034ab7c4b4b97e99520d880cdcafd9f3bf7ce6d7
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 08:21:05 -0800

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  int print_last_digit(int) - function
+ * @n: an inputted letter
+ *
+ * Return: n as it is the last digit of the no
+ */
+int print_last_digit(int n);
+	{
+	n = n % 10;
+	return (n);
+	}
