172dee5b28f867c4bb9d52cdfea00100cd5a2355
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 09:00:25 -0800

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  int print_last_digit - function
+ * print_last_digit - function
  * @n: an inputted letter
  *
- * Return: n as it is the last digit of the no
+ * Return: m as it is the last digit of the no
  */
 int print_last_digit(int n)
 	{
 	int m;
 
-	m = n % 10;
-	m = m * 10 + m;
+	m = n % 10
+	_putchar(m + '0');
 	return (m);
 	}
