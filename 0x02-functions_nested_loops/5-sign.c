653eb989fd73b99e70229804dc02ab1b96edb079
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 08:05:30 -0800

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - function
+ * @n: an inputted letter
+ *
+ * Return: 0 if it is 0, 1 if it is positive , -1 if it is negative
+ */
+
 int print_sign(int n)
 	{
 	if (n > 0)
 		{
-		printf("+");
+		_putchar('+');
 		return (1);
 		}
 	else if (n < 0)
 		{
-		printf("-");
+		_putchar('-');
 		return (-1);
 		}
 	else
 		{
-		printf("0");
+		_putchar('0');
 		return (0);
 		}
 	}
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-return (0);
-}
