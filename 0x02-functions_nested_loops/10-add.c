9bbad046ff16b59bf15ed7bbbe798ae393072d9c
Yonas Kahsay <yk8066@gmail.com>
2023-02-21 11:44:49 -0800

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function
+ *@a: a is argument
+ *@b: b is argument
+ *
+ * Return: m as it is the last digit of the no
+ */
+int add(int a, int b)
+	{
+	a = a + b;
+	return (a);
+	}
