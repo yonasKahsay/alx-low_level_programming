30d3df533464715034c78ebc52d67daaf6265683
Yonas Kahsay <yk8066@gmail.com>
2023-02-22 13:57:09 -0800

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 #include <time.h>
+void delay(int time)
+
+{
+
+// this function will create a pause in the runtime process for the number of milliseconds that is given in 'time'
+
+long pause;
+
+clock_t time1,time2; // data_type variable_name
+
+pause = time ; // if you want to be more accurate replace time with ' time * (CLOCKS_PER_SEC/1000); ' // i.e. 1000
+
+time2 = time1 = clock(); // the clock() returns back the number of ticks till it has been called .
+
+while( (time1-time2) < pause )
+
+{
+
+time1 = clock(); // this will initialize the current number of ticks to time1 ;
+
+}
+
+}
 /**
  * main - print putchar
  *
@@ -15,8 +38,11 @@ int main(void)
 	for (i = 0 ; i < 48 ; i++)
 	{
 		printf(", ");
+		delay(1000);
 		j += k;
+		delay(1000);
 		k += j;
+		delay(1000);
 		printf("%.0f, %.0f", j, k);
 	}
 	printf("\n");
