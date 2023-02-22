add5ca667b08ba3021d0e33d9c32d7cd5162b7d8
Yonas Kahsay <yk8066@gmail.com>
2023-02-22 12:45:39 -0800

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 	{
-	double i, j = 1, k = 2;
+	unsigned long long int i, j = 1, k = 2;
 
-	printf("%Lf, %Lf", j, k);
+	printf("%lld, %lld", j, k);
 	for (i = 0 ; i < 48 ; i++)
 	{
 		printf(", ");
 		j += k;
 		k += j;
-		printf("%Lf, %Lf", j, k);
+		printf("%lld, %lld", j, k);
 	}
 	printf("\n");
 	return (0);
