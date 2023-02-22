5ddc0c8b691612f318b44cb774aeaf9bf73f8ee0
Yonas Kahsay <yk8066@gmail.com>
2023-02-22 14:03:21 -0800

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <time.h>
-void delay(int time)
 
-{
-
-long pause;
-
-clock_t time1,time2;
-
-pause = time ;
-
-time2 = time1 = clock();
-while( (time1-time2) < pause )
-
-{
-
-time1 = clock();
-
-}
-
-}
 /**
  * main - print putchar
  *
@@ -35,11 +16,8 @@ int main(void)
 	for (i = 0 ; i < 48 ; i++)
 	{
 		printf(", ");
-		delay(1000);
 		j += k;
-		delay(1000);
 		k += j;
-		delay(1000);
 		printf("%.0f, %.0f", j, k);
 	}
 	printf("\n");
