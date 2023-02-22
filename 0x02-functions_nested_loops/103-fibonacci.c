4dc1a8fd963e7bd1894536ad2a7ace68b998ce6c
Yonas Kahsay <yk8066@gmail.com>
2023-02-22 12:20:32 -0800

    commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	long int i, j = 1, k = 2, sum = 2;
+
+	for (i = 0 ; i < 100 ; i++)
+	{
+		printf(", ");
+		j += k;
+		k += j;
+		if (j > 4000000 || k > 4000000)
+			break;
+		if (j % 2 == 0 && k % 2 == 0)
+			sum = sum + j + k;
+		else if (j % 2 == 0)
+			sum = sum + j;
+		else if (k % 2 == 0)
+			sum = sum + k;
+	}
+	printf("%ld\n", sum);
+	return (0);
+	}
