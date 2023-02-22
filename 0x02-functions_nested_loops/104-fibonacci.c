5b71a03b311ee84bd45c782788089440647302f1
Yonas Kahsay <yk8066@gmail.com>
2023-02-22 12:26:08 -0800

    commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - print putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	{
+	long int i, j = 1, k = 2;
+
+	printf("%ld, %ld", j, k);
+	for (i = 0 ; i < 48 ; i++)
+	{
+		printf(", ");
+		j += k;
+		k += j;
+		printf("%ld, %ld", j, k);
+	}
+	printf("\n");
+	return (0);
+	}
