88fa02468bf7219ac59ee8f76c72e022ca1a8002
Yonas Kahsay <yk8066@gmail.com>
2023-02-22 13:49:18 -0800

    commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,22 +11,13 @@ int main(void)
 	float j = 1, k = 2;
 	int i, m, n;
 
-	printf("%.1f, %.1f", j, k);
+	printf("%.0f, %.0f", j, k);
 	for (i = 0 ; i < 48 ; i++)
 	{
-		printf(", ");
+		printf(", "};
 		j += k;
-		for (m = 0 ; m < 100000 ; m++)
-		{
-			n = 1;
-			n = n + m;
-		}
 		k += j;
-		for (m = 0 ; m < 100000 ; m++)
-                {
-			n = n + m;
-                }
-		printf("%.1f, %.1f", j, k);
+		printf("%.0f, %.0f", j, k);
 	}
 	printf("\n");
 	return (0);
