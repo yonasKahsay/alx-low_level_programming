4ded3263c56af24b1d62f1b39697a3cd30137bb2
Yonas Kahsay <yk8066@gmail.com>
2023-02-23 01:18:06 -0800

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,11 +15,10 @@ void more_numbers(void)
 			for (i = 0 ; i < 14 ; i++)
 			{
 				if (i > 9)
-					_putchar('1');
+					_putchar('49');
 				_putchar(i % 10 + '0');
 			}
 			_putchar('\n');
 			j++;
 			}
-			_putchar('\n');
 		}
