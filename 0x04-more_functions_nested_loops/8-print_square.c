81fa62df62b9cef08b89c17d1c9f711f87139078
Yonas Kahsay <yk8066@gmail.com>
2023-02-23 02:37:19 -0800

    commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,11 +8,12 @@
  */
 void print_square(int size)
 		{
-			int i;
-			int j;
+			int i, j;
 
 			if (size <= 0)
+			{
 				_putchar('\n');
+			}
 			for (i = 0 ; i < size ; i++)
 			{
 				for (j = 0 ; j < size ; j++)
