d9e0cf95760d4173f9e908553f605cce393b6a9e
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 08:57:06 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - This function prints a string
+ * @s: inputted string
+ *
+ * Return: it returns nothing
+ */
+void print_rev(char *s)
+		{
+		int i;
+		int j;
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+		if ((*(str + i)) == '\0')
+		break;
+		}
+		while (i > -1)
+		{
+			_putchar((*(str + i));
+				i--;
+		}
+		 _putchar('\n');
+		}
