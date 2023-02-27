8e48d4f1c544068b797d3431b5b222d3c9f09a6e
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 04:46:25 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - This function updateds the vallue it points to 98
+ * @s: first no
+ *
+ * Return: it returns nothing
+ */
+int _strlen(char *s)
+		{
+		int len =strlen(*s);
+		return (len);
+		}
