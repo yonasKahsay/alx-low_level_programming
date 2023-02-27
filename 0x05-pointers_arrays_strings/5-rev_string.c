f83ad7c8f4bc8d6ee46b8b205a8fa748b9faaea9
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 09:13:48 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - This function reverses a string
+ * @s: inputted string
+ *
+ * Return: it returns nothing
+ */
+void rev_string(char *s)
+		{
+		int i, j = 0;
+		char c[1000];
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+		if ((*(s + i)) == '\0')
+		break;
+		c[i] = (*(s + i));
+		}
+		i--;
+		while (i >= 0)
+		{
+		*(s + j) = c[i];
+			j++;
+			i--;
+		}
+		}		}
