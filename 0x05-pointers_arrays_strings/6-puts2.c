a16897cd4bad34d1b02c0580989bc9b1335c7c2a
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 09:32:44 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - This function print every other string
+ * @str: inputted string
+ *
+ * Return: it returns nothing
+ */
+void puts2(char *str)
+		{
+		int i, j = 0;
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+		if (((*(s + i)) == '\0' || *(s + i - 1)) == '\0')
+		break;
+		_putchar((*(s + i)));
+		i = i + 2;
+		}
+		}
