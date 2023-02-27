fb842fc859f5ec244d2ba10c67cc8fbf2e0113ec
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 10:03:04 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - This function prints a string
+ * @s: inputted string
+ *
+ * Return: it returns nothing
+ */
+void puts_half(char *str)
+		{
+		int i, j;
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+		if ((*(s + i)) == '\0')
+		break;
+		}
+		i--;
+		if(i % 2 == 0)
+		{
+			j = i / 2;
+			while(j <= i)
+			{
+				_putchar((*(s + j)));
+				j++;
+			}
+		}
+		else
+		{
+			j = ((i -1) / 2);
+			while(j <= i)
+                        {
+                                _putchar((*(s + j)));
+                                j++;
+                        }
+		}
+		 _putchar('\n');
+		}
