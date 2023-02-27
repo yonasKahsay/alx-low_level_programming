8be70ccfbf8f6224e8c8ade4057f8fe1d886d4be
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 12:29:24 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - This function prints a string
+ * @dest: inputted array
+ * @src: length of the array
+ *
+ * Return: it returns nothing
+ */
+char *_strcpy(char *dest, char *src)
+		{
+		int i;
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+			*(dest + i) = *(src + i);
+			if ( *(src + i) == '\0')
+				break;
+		}
+			 return (*dest);
+		}
