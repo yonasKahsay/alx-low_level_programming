2adfb7b72400325405c95e152b5c6ae3591c9892
Yonas Kahsay <yk8066@gmail.com>
2023-02-27 09:38:22 -0800

    commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,7 @@ void puts2(char *str)
 		{
 		int i;
 
-		for (i = 0 ; i < 1000 ; i + 2)
+		for (i = 0 ; i < 1000 ; i = i + 2)
 		{
 		if (((*(str + i)) == '\0' || *(str + i - 1)) == '\0')
 		break;
