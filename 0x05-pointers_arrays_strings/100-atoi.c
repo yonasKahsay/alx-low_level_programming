474e56242b1148ba11fab591aed48ec114e3de76
Yonas Kahsay <yk8066@gmail.com>
2023-02-28 08:56:34 -0800

    commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,21 @@
 int _atoi(char *s)
 		{	
 			unsigned int i;
-			int l;
+			int l = 0, d;
 
 		for (i = 0 ; i < 1000 ; i++)
 		{
 			if ((*s + i) <= 57 && (*s + i) >= 48)
-				break;
+				for (j = 1 ; j < 100 ; j++)
+				{
+					if ((*s + (i + j)) >= 57 && (*s + (i + j)) <= 48)
+						goto lastline;
+				}
+			break;
 		}
-		l = (*s + i) - '0';
+lastline:
+		d = j;
+		for (j = 1 ; j < d ; j++)
+		l = l + ((*s + (i + j)) - '0') * 10**(d - j) ;
 		return (l);
 		}
