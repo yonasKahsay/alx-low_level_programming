d85e79e46a29aa180d6e0b30fdf777de7110fe07
Yonas Kahsay <yk8066@gmail.com>
2023-02-28 08:06:44 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - This function prints a string
+ * @s: inputted array
+ *  
+ * Return: it returns a number
+ */
+int _atoi(char *s)
+		{	
+			unsigned int i;
+			int l;
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+			if ((*s + i) <= 57 && (*s + i) >= 48)
+				break;
+		}
+		l = (*s + i);
+		return (l);
+		}
