ebf625a00b3e0883cdf9eacbb9c81e788740adf2
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 06:10:13 -0800

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,22 +10,14 @@
  */
 int _strcmp(char *s1, char *s2)
 		{
-		int i, j;
+		int i;
 
 		for (i = 0 ; i < 1000000 ; i++)
 		{
-		if (s1[i] == '\0')
-		break;
+		if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
 		}
-		j = 0;
-		while (s2[j] != '\0')
-		{
-			j++;
+		return( 0 );
 		}
-			if (i < j)
-			return (-15);
-			else if (i == j)
-			return (0);
-			else
-			return (15);
-				}
