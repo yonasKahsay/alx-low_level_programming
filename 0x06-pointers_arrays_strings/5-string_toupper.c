1ebdd946134d54685269060386be4042319d0336
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 07:49:54 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *string_toupper - This funtion returnsan integer based on the
+ * @c: inputted string
+ *
+ * Return: it returns a converted string
+ */
+char *string_toupper(char *c)
+		{
+		int i = 0, j;
+		
+		while (c[i] != 0)
+		{
+			if ( c[i] > 96 && c[i] < 123)
+		 	c[i] = c[i] - '32'
+				i++;
+		}
+		return (c);
+		}
