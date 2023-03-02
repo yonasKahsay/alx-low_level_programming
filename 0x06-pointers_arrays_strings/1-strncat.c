ed7fe0e276bccfd384146eb2e24b33d1bb5fafbf
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 00:34:22 -0800

    commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,21 +19,12 @@ char *_strncat(char *dest, char *src, int n)
 		break;
 		}
 		j = 0;
-		while (j < n)
+		while (j < n && *(src + j) !=0)
 		{
-			if ((*(src + j)) == '\0' )
-                {
-			while (j < n)
-			{
-                        (*(dest + i)) = '\0';
-			j++;
-			i++;
-			}
-                        break;
-		}
 			(*(dest + i)) = (*(src + j));
 			j++;
 			i++;
 		}
+			(*(dest + i)) = '\0';
 			return (dest);
 		}
