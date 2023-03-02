7caa1d164cfc8c21f130c28b119b3923498d8350
Yonas Kahsay <yk8066@gmail.com>
2023-03-01 22:10:36 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strncat - This function concatenates two strings
+ * @src: inputted string
+ * @dest: inputted string
+ * @n: inputted no of string to concatenate
+ *
+ * Return: it returns a concattenated string
+ */
+char *_strncat(char *dest, char *src, int n)
+		{
+		int i, j;
+
+		for (i = 0 ; i < 1000 ; i++)
+		{
+		if ((*(dest + i)) == '\0')
+		break;
+		}
+		j = 0;
+		while (j < n)
+		{
+			(*(dest + i)) = (*(src + j));
+			j++;
+			i++;
+		}
+		return (dest);
+		}
