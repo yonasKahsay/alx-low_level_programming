36914823359b26402cb160f71f10640779082904
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 08:09:46 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *cap_string - This funtion returnsan integer based on the
+ * @c: inputted string
+ *
+ * Return: it returns a converted string
+ */
+char *cap_string(char *c)
+		{
+		int i = 0;
+
+		while (c[i] != 0)
+		{
+			if (c[i] > 96 && c[i] < 123)
+				c[i] = c[i] - 32;
+			i++;
+		}
+		return (c);
+		}
