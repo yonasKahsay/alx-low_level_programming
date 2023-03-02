1e451f16960c85c2c9bff377ed5d9abd1982fb7b
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 09:22:35 -0800

    commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,17 +14,17 @@ char *leet(char *c)
 		while (c[i] != '\0')
 		{
 			if (c[i] == 'a' || c[i] == 'A')
-				c[i] = 4;
+				c[i] = '4';
 			else if (c[i] == 'e' || c[i] == 'E')
-				c[i] = 3;
+				c[i] = '3';
 			else if (c[i] == 'o' || c[i] == 'O')
-				c[i] = 0;
+				c[i] = '0';
 			else if (c[i] == 't' || c[i] == 'T')
-				c[i] = 7;
+				c[i] = '7';
 			else if (c[i] == 'l' || c[i] == 'L')
-				c[i] = 1;
+				c[i] = '1';
 			else
-
+				c[i] = c[i];
 			i++;
 		}
 		return (c);
