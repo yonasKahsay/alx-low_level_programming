df9a894640ea3d4cec6ab45dbcee087f2265ee0d
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 11:42:19 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *rot13 - This funtion returnsan integer based on the
+ * @c: inputted string
+ *
+ * Return: it returns a converted string
+ */
+char *rot13(char *c)
+		{
+		int i, j;
+		char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+		char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+		while (c[i] != '\0')
+		{
+			for (j = 0 ; j < 52 ; j++)
+			{
+				if (c[i] == input[j])
+				{
+					c[i] = output[];
+				}
+			}
+				i++;
+		}
+		return (c);
+		}
