f682fba1f74db75f365390674f75cad420eb7ca4
Yonas Kahsay <yk8066@gmail.com>
2023-03-02 06:25:09 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - This funtion returnsan integer based on the  comparison.
+ * @a: inputted string
+ * @n: inputted string
+ *
+ * Return: it returns an integer
+ */
+void reverse_array(int *a, int n)
+		{
+		int i;
+		int b [n];
+
+		b = a;
+		for (i = 0 ; i < n ; i++)
+		{
+			a[i] = b[n];
+			n--;
+		}
+		}
