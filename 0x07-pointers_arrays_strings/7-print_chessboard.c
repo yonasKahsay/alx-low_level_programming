f146414e3411e64bb3715d4625b798a4dcdff89b
Yonas Kahsay <yk8066@gmail.com>
2023-03-06 11:33:56 -0800

    commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,11 +11,11 @@ void print_chessboard(char (*a)[8])
 		{
 		 int i, j;
 
-		for (i = 0 ; i < a ; i++)
+		for (i = 0 ; i < *a ; i++)
 		{
 			for (j = 0; j < 8 ; j++)
 			{
-				putchar(a[i][j]);
+				_putchar(a[i][j]);
 			}
 			_putchar('\n');
 		}
