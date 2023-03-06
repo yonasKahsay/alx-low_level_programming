4f2bc12aea8b548209f79182d5a2f05c63297859
Yonas Kahsay <yk8066@gmail.com>
2023-03-06 04:18:18 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strpbrk - returns the pointer to where the accept string has the same val
+ * @s: pointer to  data of string
+ * @accept: character to match to
+ *
+ * Return: it returns a pointer to the first appearance of the letter c
+ */
+char *_strpbrk(char *s, char *accept)
+		{
+		 int i = 0, j;
+
+		while (s[i] != '\0')
+		{
+			j = 0;
+			while (accept[j] != '\0')
+			{
+				if (s[i] == accept[j])
+				goto lastline;
+				j++;
+			}
+			i++;
+		}
+lastline:
+			if (s[i] == '\0' && accept[j] != '\0')
+		{
+			return (NULL);
+		}
+		return (s + i);
+		}
