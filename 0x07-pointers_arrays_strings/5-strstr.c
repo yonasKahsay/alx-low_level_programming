4549ec9a274db98d52c5495f2930cda10e7d7838
Yonas Kahsay <yk8066@gmail.com>
2023-03-06 05:07:45 -0800

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,7 +19,7 @@ char *_strstr(char *haystack, char *needle)
 			{
 				if (haystack[i] == needle[j])
 					x++;
-				if (haystack[i] == needle[j] && x == 2)
+				if (haystack[i] == needle[j] && x == 4)
 				{
 				goto lastline;
 				}
