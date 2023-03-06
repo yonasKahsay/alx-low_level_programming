fe94cc7e6bad3775370b6ce5f92bda18c482ea20
Yonas Kahsay <yk8066@gmail.com>
2023-03-06 11:30:33 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard - prints an elements of an array
+ * @a: pointer to  data of array
+ *
+ * Return: it returns a pointer to the first appearance of the letter c
+ */
+void print_chessboard(char (*a)[8])
+		{
+		 int i, j;
+
+		for (i = 0 ; i < a ; i++)
+		{
+			for (j = 0; j < 8 ; j++)
+			{
+				putchar(a[i][j]);
+			}
+			_putchar('\n');
+		}
+		}
