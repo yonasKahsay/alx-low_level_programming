20cd32a9a268c037a4289876eb07051a4cff3459
Yonas Kahsay <yk8066@gmail.com>
2023-03-08 12:01:28 -0800

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -25,8 +25,8 @@ int is_prime_number(int n)
 int find_prime(int n, int i)
 		{
 			if (i >= n)
-                          	return (1);
+				return (1);
 			if ((n % i) == 0)
 				return (0);
-			return (find_prime(n, i++));
+			return (find_prime(n, i + 1));
 		}
