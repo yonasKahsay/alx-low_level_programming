b984fcf16a0b430519412b0b08435dac56ee2733
Yonas Kahsay <yk8066@gmail.com>
2023-03-08 14:58:33 -0800

    commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -29,12 +29,12 @@ int wildcmp(char *s1, char *s2)
 int check_wildcmp(char *s1, char *s2, char *e1, char *e2)
 		{
 			if (*s2 == '*' && *e2 == '*')
- 				return (1);
+				return (1);
 			if (*s1 == *s2 && *e1 == *e2)
 				return (1);
- 			if ((*s1 != *s2 || *e1 != *e2) && (*s2 != '*' || *e2 != '*'))
- 				return (0);
 			if(*s2 == '*' && *e2 == *e1)
 				return (1);
+			if ((*s1 != *s2 || *e1 != *e2) && (*s2 != '*' || *e2 != '*'))
+				return (0);
 			return (5);
 		}
