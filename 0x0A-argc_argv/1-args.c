67199790d0e869219c39897ed558c5a743a33256
Yonas Kahsay <yk8066@gmail.com>
2023-03-10 01:49:13 -0800

    commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: integer value counting the number of arguments
+ * @argv: array of strings
+ *
+ * Return: it returns 0 if the program compiles ok
+ */
+int main(__attribute__((unused))int argc,__attribute__((unused)) char *argv[])
+		{
+		printf("%d\n", argc);
+		return (0);
+		}
