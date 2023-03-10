4eade67120ae0a9ba0409ea578da10f797a6f0d7
Yonas Kahsay <yk8066@gmail.com>
2023-03-10 01:38:53 -0800

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - calculates the square root of a number
+ * @argc: integer value counting the number of arguments
+ *
+ * Return: it returns 0 if the program compiles ok
+ */
+int main (__attribute__((unused))int argc, char* argv[])
+		{
+		printf("%s", argv[0]);
+		return (0);
+		}
