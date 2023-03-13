34e6f374f5c4a1351e59665376b35fc0bfb333ff
Yonas Kahsay <yk8066@gmail.com>
2023-03-13 06:45:57 -0700

    commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - copies a given string to another location
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: it returns pointer to copied string
+ */
+char *str_concat(char *s1, char *s2)
+		{
+		unsigned int i = 0, j = 0, k = 0;
+		char *ret;
+
+		while (s1[i] != 0)
+		{
+		i++;
+		}
+		while (s2[j] != 0)
+		{
+		j++;
+		}
+		ret = malloc(i + j + 2);
+		if (ret == NULL)
+		return (NULL);
+		for (k = 0; k < i ; k++)
+		{
+		ret[k] = s1[k];
+		}
+		for (i = 0 ; i <= j ; i++)
+		{
+		ret[k] = s2[i];
+		k++;
+		}
+		return (ret);
+		}
