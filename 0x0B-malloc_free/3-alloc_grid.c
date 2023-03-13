2b5118ccad4e38e19360cf96eea25e1fe6b29455
Yonas Kahsay <yk8066@gmail.com>
2023-03-13 15:01:19 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 
 		if (width <= 0 || height <= 0)
 			return (NULL);
-		ret = malloc(4 * height * width);
+		ret = malloc(height * width);
 		if (ret == NULL)
 		{
 			free(ret);
