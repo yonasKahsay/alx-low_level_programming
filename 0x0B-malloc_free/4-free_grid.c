81c55037ff72f4de05f9d57d561eb035f9a61602
Yonas Kahsay <yk8066@gmail.com>
2023-03-14 13:08:09 -0700

    commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees the memory hold by the grid.
+ * @grid : the grid size
+ * @height: the height for an array.
+ *
+ * Return: it returns noting
+ */
+void free_grid(int **grid, int height)
+		{
+		int i = 0, j = 0;
+
+		if (grid === NULL || height == 0)
+			return;
+		for(i = 0 ; i <= height ; i++)
+			free(grid[i];
+		}
