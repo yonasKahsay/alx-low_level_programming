a45c3455cdc5dee769d785c4a70a62f974ff2152
Yonas Kahsay <yk8066@gmail.com>
2023-03-14 13:09:25 -0700

    commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,10 +10,10 @@
  */
 void free_grid(int **grid, int height)
 		{
-		int i = 0, j = 0;
+		int i = 0;
 
-		if (grid === NULL || height == 0)
+		if (grid == NULL || height == 0)
 			return;
 		for(i = 0 ; i <= height ; i++)
-			free(grid[i];
+			free(grid[i]);
 		}
