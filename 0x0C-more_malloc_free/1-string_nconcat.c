cf5b7d90e5e5fb8ba8cfb91137011dbfb2aa676f
Yonas Kahsay <yk8066@gmail.com>
2023-03-16 03:09:44 -0700

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		}
 		if (n >= j)
 			n = j - 1;
-		ret = malloc(sizeof(char) * (i + n + 1));
+		ret = malloc(sizeof(char) * (i + n + 5));
 		if (ret == NULL)
 			return (NULL);
 		for (k = 0 ; k <= (n + i) ; k++)
