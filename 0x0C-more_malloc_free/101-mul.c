bb72b99fb572884529f02ec5bfb9392e9a7a556f
Yonas Kahsay <yk8066@gmail.com>
2023-03-16 14:14:28 -0700

    commit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ * @argc: integer value counting the number of arguments
+ * @argv: array of strings
+ *
+ * Return: it returns 0 if the program compiles ok
+ */
+int main(int argc, char *argv[])
+		{
+		int m = 0, i;
+
+		if (argc != 3)
+		{
+		printf("Error\n");
+		exit(98);
+		}
+		for (i = 1 ; i < argc ; i++)
+		{
+		unsigned long int j = 0;
+
+		for (j = 0; j < strlen(argv[i]) ; j++)
+		{
+		if (isdigit(argv[i][j]) == 0)
+		{
+		printf("Error\n");
+		exit(98);
+		}
+		}
+		}
+		m = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", m);
+		return (0);
+		}
