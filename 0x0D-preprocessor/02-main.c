b0de825707134821600ae86c44c36f0a32fdb982
Yonas Kahsay <yk8066@gmail.com>
2023-03-20 02:04:39 -0700

    preprocesser

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - entry point
+*
+*
+* return: it returns 0 when exit
+*/
+
+int main(void)
+	{
+	printf("%s\n", __FILE__);
+	return (0);
+	} 
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* main - entry point
+*
+* Return: it returns 0 when exit
+*/
+int main(void)
+	{
+	printf("%s\n", __FILE__);
+	return (0);
+	}
