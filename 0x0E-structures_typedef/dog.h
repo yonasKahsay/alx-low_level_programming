6b7a548353e0851621ee0b1203388ae67a55e196
Yonas Kahsay <yk8066@gmail.com>
2023-03-20 02:40:02 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,9 @@
+#ifndef dog_h
+#define dog_h
+struct dog
+		{
+		char *name;
+		float age;
+		char *owner;
+		};
+#endif
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ *main.h
+ *prototypes for functions
+ */
+int _putchar(char c);
+#endif
