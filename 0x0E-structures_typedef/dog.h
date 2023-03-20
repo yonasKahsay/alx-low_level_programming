385919a338b2f3857be24d590972078226e3daa7
Yonas Kahsay <yk8066@gmail.com>
2023-03-20 03:52:50 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef dog_h
 #define dog_h
+/**
+* struct dog - initializes a structure
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/ 
 struct dog
 		{
 		char *name;
