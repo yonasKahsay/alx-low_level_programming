1b69855f2131d3f024ff75225fa2c6746639fff9
Yonas Kahsay <yk8066@gmail.com>
2023-03-20 12:16:11 -0700

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,14 @@
 void print_dog(struct dog *d)
 		{
 		if (d == NULL)
-			return ;
+			return;
 		if (d->name == NULL)
 			printf("Name: (nil)\n");
-		else 
+		else
 			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
 			printf("owner: %s\n", d->owner);
-		return ;
 		}
