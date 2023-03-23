f6967bec9d785b40b04d3b04d5ea3790d42de7e0
Yonas Kahsay <yk8066@gmail.com>
2023-03-23 08:29:26 -0700

    One woman

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - prints the strings passed
+ * @n: number of argument
+ * @separator: separator character for the string
+ *
+ * Return: it returns nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+		{
+		va_list argu;
+		unsigned int i;
+		char *c;
+
+		va_start(argu, n);
+		for (i = 0 ; i < n ; i++)
+		{
+			c = va_arg(argu, char *);
+			if (c == NULL)
+			printf("nil");
+			printf("%s", c);
+			if (i == (n - 1))
+				break;
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+			printf("\n");
+		}
