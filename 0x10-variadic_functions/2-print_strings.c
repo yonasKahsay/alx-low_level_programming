fc0b976007608dfebea67ca2ef37c83a220c7099
Yonas Kahsay <yk8066@gmail.com>
2023-03-23 08:31:32 -0700

    One woman

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			c = va_arg(argu, char *);
 			if (c == NULL)
-			printf("nil");
+				printf("nil");
+			else
 			printf("%s", c);
 			if (i == (n - 1))
 				break;
