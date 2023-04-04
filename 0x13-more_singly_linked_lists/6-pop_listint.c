afac4fb46ac9419917607756cc1e69ea983be03c
YonasKahsay <yk8066@gmail.com>
2023-04-03 22:38:59 -0700

    7. Get node at index

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,7 +13,7 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL || *head == NULL)
-		return(0);
+		return (0);
 	data = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - deletes the head node of the linked list
+ * @head: head of the list
+ * @index: index at node
+ *
+ * Return: returns node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int node;
+
+	if (head == NULL)
+		return (NULL);
+	while (head->next != NULL)
+		{
+		head = head->next;
+		node++;
+		if (index == node)
+			break;
+		}
+	return (head);
+}
