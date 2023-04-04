2ac67bbc434b8b47c43232b648a2f9a6e2d4f3b3
YonasKahsay <yk8066@gmail.com>
2023-04-04 07:35:38 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,7 +4,7 @@
 /**
  * insert_nodeint_at_index - deletes the head node of the linked list
  * @head: head of the list
- * @inx: index at node
+ * @idx: index at node
  * @n: value of data at an index
  *
  * Return: returns node
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int node = 0;
 	listint_t *temp = *head, *new, *temp2;
-	
+
 	new = malloc(sizeof(listint_t));
 		if (new == NULL)
 		return (NULL);
@@ -38,5 +38,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (new);
 		}
 		}
+		if (temp->next == NULL && idx == node)
+			{
+			temp2 = temp->next;
+			temp->next = new;
+			new->n = n;
+			new->next = temp2;
+			return (new);
+			}
 	return (NULL);
 }
