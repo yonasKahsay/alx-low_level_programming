8474894b08ae38d1c067fdea034d920b2d6baedd
YonasKahsay <yk8066@gmail.com>
2023-04-04 07:37:24 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head)->next = new;
 		return (new);
 		}
-		node = 1;
 	while (temp->next != NULL)
 		{
 		node++;
