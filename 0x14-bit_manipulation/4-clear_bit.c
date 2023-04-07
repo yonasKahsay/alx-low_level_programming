3d56780b305a10e57a33c5fba4e5b3cafc88315e
YonasKahsay <yk8066@gmail.com>
2023-04-07 13:13:21 -0400

    Update 4-clear_bit.c
    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* set_bit - sets to one at selected index
+* clear bit - clears to 0 at selected index
 * @n: number to print in binary
 * @index: index to change
 * Return: Returns 1 or -1
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
   if (index >= sizeof(unsigned long int) * 8)
       return (-1);
