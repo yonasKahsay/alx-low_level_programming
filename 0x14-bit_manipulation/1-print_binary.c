3a85cdae605ddf85764b9428b2ab81608e698bfc
YonasKahsay <yk8066@gmail.com>
2023-04-07 12:40:03 -0400

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
@@ -9,7 +7,7 @@
  */
 void print_binary(unsigned long int n)
 {
-    if (n > 1) 
+    if (n > 1)
     {
         print_binary(n >> 1);
     }
