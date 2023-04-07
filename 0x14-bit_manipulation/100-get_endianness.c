958656d640e5db6b4d717c6f3c257fa7c4c15bdd
YonasKahsay <yk8066@gmail.com>
2023-04-07 14:43:10 -0400

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
 * get_endianness - checks for endianness
-* 
+* @void: nothing
+*
 * Return: Returns 1 or zero
 */
-int get_endianness()
+int get_endianness(void)
 {
   int n = 1;
   char *endian = (char*)&n;
