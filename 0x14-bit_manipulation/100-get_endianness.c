8e5060730a51bca463587f0d0481a860c9c43b55
YonasKahsay <yk8066@gmail.com>
2023-04-07 13:29:38 -0400

    Create 100-get_endianness.c
    6. Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* get_endianness - checks for endianness
+* 
+* Return: Returns nothing
+*/
+int get_endianness(void)
+{
+  int n = 1;
+  char *endian = (char*) & n ;
+  if(*endian == 1)
+    return (1);
+  return (0);
+}
