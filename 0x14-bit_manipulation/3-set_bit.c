da931620b3bc4fd272c2d5a1393a3767add2491b
YonasKahsay <yk8066@gmail.com>
2023-04-07 13:04:41 -0400

    Update and rename 4-clear_bit.c to 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* print_binary - prints the binary equivalent of a decimal number
+* set_bit - sets to one at selected index
 * @n: number to print in binary
 * @index: index to change
 * Return: Returns 1 or -1
 */
-int clear_bit(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
   if (index >= sizeof(unsigned long int) * 8)
       return (-1);
