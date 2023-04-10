d7b6ba0f3dcb903314d6a9952fb505470def544b
YonasKahsay <yk8066@gmail.com>
2023-04-10 15:44:18 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 					if (text_content != NULL)
 					{
 						length = strlen(text_content);
-						fwrite(text_content , sizeof(char) , length, file);
+						fwrite(text_content, sizeof(char), length, file);
 					}
 					fclose(file);
 					return (1);
