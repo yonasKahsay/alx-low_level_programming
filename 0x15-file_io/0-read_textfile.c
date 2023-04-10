ff8911709d42ef0b4225c9c751d57d1a996ccf85
YonasKahsay <yk8066@gmail.com>
2023-04-10 07:26:30 -0700

    0. Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+/**
+ * read_textfile - Print binary number
+ * @filename: The name of the file
+ * @letters: The number of letters to be read and printed
+ *
+ * Return: returns the number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+	{
+		FILE *fp =  NULL;
+		char ch;
+		size_t count = -1;
+		ssize_t count2 = 0;
+
+		if (filename == NULL)
+			return (0);
+		fp = fopen(filename, "r");
+		if (fp == NULL)
+			return (0);
+		while (feof(fp) != 1)
+			{
+			ch = fgetc(fp);
+			count++;
+			if (count == (letters))
+			{
+			putchar(ch);
+			break;
+			}
+			putchar(ch);
+			}
+		fclose(fp);
+		count2 = count;
+		return (count2);
+	}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ *main.h
+ *prototypes for functions
+ */
+int  _putchar(char c);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+int append_text_to_file(const char *filename, char *text_content);
+#endif
